59e5cef5528d65f0abaef92b5f8e05d72c7870f2
guilhermecosta21-dev <gcosta21.dev@gmail.com>
2026-09-11 09:12:41 -0300

    feat(documentos): salvando arquivo no upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -35,6 +35,11 @@ namespace BancoSENAIAPI.Controllers
             string nameOriginal = Path.GetFileNameWithoutExtension(arquivo.FileName);
             string novoNome = $"{codigoCliente}_{nameOriginal}_{Guid.NewGuid()}{extensao}";
             string caminhoFinal = Path.Combine(pastaCliente, novoNome);
+
+            using (var stream = new FileStream(caminhoFinal, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
         }
     }
 }
