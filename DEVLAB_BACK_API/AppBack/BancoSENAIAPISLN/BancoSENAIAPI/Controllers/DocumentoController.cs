5be8283395201e65b6eacf80881a807a840583bf
guilhermecosta21-dev <gcosta21.dev@gmail.com>
2026-09-11 08:38:49 -0300

    feat(documentos) simulacao do banco de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -7,5 +7,13 @@ namespace BancoSENAIAPI.Controllers
     public class DocumentoController : Controller
     {
 
+        private readonly string _caminhoRaiz = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "ClienteArquivos"
+            );
+
+        private static List<Models.DocumentoMetadado> _documentosMetadados  = new List<Models.DocumentoMetadado>();
+
+        private static int _nextId = 1;
     }
 }
