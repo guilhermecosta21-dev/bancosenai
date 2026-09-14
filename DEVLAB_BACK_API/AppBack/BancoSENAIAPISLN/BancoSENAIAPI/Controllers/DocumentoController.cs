c96e3a3a2a919edaad42f743df59f89e609f1553
guilhermecosta21-dev <gcosta21.dev@gmail.com>
2026-09-14 08:41:42 -0300

    feat(documentos): exclusao

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -88,6 +88,26 @@ namespace BancoSENAIAPI.Controllers
             string nomeArquivo = $"{arquivo.Name}{arquivo.Extensao}";
 
             return File(fileBytes, "application/octet-stream", nomeArquivo);
-        } 
+        }
+
+        [HttpDelete("excluir/{id}")]
+        public IActionResult ExcluirArquivo(int id) 
+        {
+            var arquivo = _documentosMetadados.FirstOrDefault(d => d.Id == id);
+
+            if(arquivo == null)
+            {
+                return NotFound(new { mensagem = "Documento não encontrado." });
+            }
+
+            if (System.IO.File.Exists(arquivo.Caminho))
+            {
+                System.IO.File.Delete(arquivo.Caminho);
+            }
+
+            _documentosMetadados.Remove(arquivo);
+
+            return Ok(new { mensagem = "O documento e arquivo foram excluídos com sucesso." });
+        }
     }
 }
